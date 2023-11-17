d2069a6112a5aba0f3e34036299b2c53e23bef62
eddbot <edwardsansome@gmail.com>
2023-11-17 00:01:41 +0000

    funfunfun :)

---
+++ Fantasy/MemoryAccessor.cs
@@ -0,0 +1,54 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Fantasy;
+
+public class MemoryAccessor : IDisposable
+{
+private readonly int _processAllAccess = 0x1F0FFF;
+
+private readonly IntPtr _handle;
+
+public MemoryAccessor(int pid)
+{
+
+    _handle = Open(pid);
+    if (_handle == IntPtr.Zero)
+    {
+        throw new ApplicationException("unable to get handle");
+    }
+
+}
+
+[DllImport("Kernel32.dll")]
+private static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+[DllImport("Kernel32.dll")]
+private static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [Out] byte[] lpBuffer, uint nSize, out int lpNumberOfBytesRead);
+
+[DllImport("Kernel32.dll")]
+private static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [Out] byte[] lpBuffer, uint nSize, out int lpNumberOfBytesWritten);
+
+
+[DllImport("Kernel32.dll")]
+private static extern bool CloseHandle(IntPtr hObject);
+
+
+public IntPtr Open(int pid) => OpenProcess(_processAllAccess, false, pid);
+
+public bool Read()
+{
+    return true;
+}
+
+public bool Write(IntPtr address, [Out] byte[] buffer, out int bytesWritten)
+{
+    return WriteProcessMemory(_handle, address, buffer, (uint) buffer.Length, out bytesWritten);
+}
+
+
+public void Dispose()
+{
+    CloseHandle(_handle);
+}
+}
---
+++ Fantasy/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using Fantasy;
+
+
+var process = Process.GetProcessesByName("ff7_en").FirstOrDefault() ??
+              throw new ApplicationException("can't find the ff7 process!");
+
+using var mem = new MemoryAccessor(process.Id);
+
+// Cloud starts here
+var cloudBaseAddress = new IntPtr(0xDBFD8D);
+
+// Change the level
+var cloudLevelAddress = IntPtr.Add(cloudBaseAddress, 0);
+byte level = 69;
+var levelBuffer = new byte[1] { level };
+Console.WriteLine(mem.Write(cloudLevelAddress, levelBuffer, out _) ? "Level updated?" : "mem not written");
+
+// Change the strength
+var cloudStrengthAddress = IntPtr.Add(cloudBaseAddress, 1);
+byte strength = 199;
+var strengthBuffer = new byte[1] { strength };
+Console.WriteLine(mem.Write(cloudStrengthAddress, strengthBuffer, out _) ? "Strength updated?" : "mem not written");
+
+// Change the name
+var cloudNameAddress = IntPtr.Add(cloudBaseAddress, 15);
+var funnyName = new byte[10] { 48, 37, 46, 41, 51, 255, 255, 255, 255, 255 };
+var cloudName = new byte[10] { 35, 76, 79, 85, 68, 255, 255, 255, 255, 255 };
+Console.WriteLine(mem.Write(cloudNameAddress, funnyName, out _) ? "Name Written?" : "mem not written");
